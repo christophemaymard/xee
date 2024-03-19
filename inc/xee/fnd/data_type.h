304408ffb5fcb850cdbd5144d059c11e0adf065f
Christophe Maymard <christophe.maymard@hotmail.com>
2024-03-19 13:02:20 +0100

    Created data types.

---
+++ inc/xee/fnd/data_type.h
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: Copyright (c) 2024, Christophe Maymard <christophe.maymard@hotmail.com>
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef __XEE_FND_DATA_TYPE_H__
+#define __XEE_FND_DATA_TYPE_H__
+
+#include <cstddef>
+#include <cstdint>
+
+//==============================================================================
+
+//------------------------------------------------------------------------------
+
+using s8 = std::int8_t;
+using s16 = std::int16_t;
+using s32 = std::int32_t;
+using s64 = std::int64_t;
+
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
+using usize = std::size_t;
+
+using f32 = float;
+using f64 = double;
+
+#endif // #ifndef __XEE_FND_DATA_TYPE_H__
+
